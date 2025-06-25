5ae4145ff2dde7f3a4a5054d6bd489cc4c79092b
smp365-create <seamealprep365@gmail.com>
2025-06-25 08:17:35 -0700

    Update mainGame.js

--- js/mainGame.js
+++ js/mainGame.js
@@ -265,21 +265,21 @@ const gameData = {
   choices: {
     "Take a Screenshot and Share!": [0, []]
   }
-}
+},
 "28": {
   text: "Behold your coronation accessory!",
   image: "smaller_images/R1.png",
   choices: {
     "Take a Screenshot and Share!": [0, []]
   }
-}
+},
 "29": {
   text: "Behold your coronation accessory!",
   image: "smaller_images/R1.png",
   choices: {
     "Take a Screenshot and Share!": [0, []]
   }
-}
+},
  
   "0": {}
 };
