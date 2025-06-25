2b221cce9dc44ef433ea6e388e5da1aae2dd195d
smp365-create <seamealprep365@gmail.com>
2025-06-25 12:48:34 -0700

    Update mainGame.js

--- js/mainGame.js
+++ js/mainGame.js
@@ -200,8 +200,8 @@ const gameData = {
     }
   },
 "21": {
-  text: "Your secret hideaway in the palace is...",
-  image: "smaller_images/A1.png",
+  text: "Drumroll.....Here is Your Royal ID",
+  image: "smaller_images/Z1.png",
   choices: {
     "A glass conservatory filled with orchids": [24, []],
     "A candle-lit tower room with velvet cushions": [24, []],
